1b6ff91aad0abea05d5a715039cceb60bfb015d1
Dharma106 <20860882+Dharma106@users.noreply.github.com>
2018-04-13 18:08:42 +0530

    Update Percentage_Cont_Bar_Graph.R

--- Percentage_Cont_Bar_Graph.R
+++ Percentage_Cont_Bar_Graph.R
@@ -1,16 +1,32 @@
+# To install packages which are not there in the system
+req_pkgs <- c("ggplot2", "readxl", "reshape2", "grid")
+# if packages exists it will evalute to character(0).
+pkgs_avail <- req_pkgs[!(req_pkgs %in% installed.packages())]
+
+if(length(pkgs_avail)){
+  install.packages(pkgs_avail)
+}
+
+library(readxl)
 library(reshape2)
 library(ggplot2)
+library(grid)
 
-cost_component_graph <- function(data, year){
+
+cost_component_graph <- function(data, year, title_name){
+  # Exit function if mentioned variable is missing.
+  if(missing(data)){
+    stop("Data is missing with no default")
+  }
   # Counting total rows & Read variable names
   total_row <- nrow(data)
   col_names <- colnames(data)
   # Exit function if mentioned variable is missing.
   if((is.null(which(col_names == "Inputs"))))
-    stop("Input Components Column is missing")
+    stop("Input Component Column is missing")
   # To check whether user has provided the arguments or not
   if(missing(year)){
-    year <- readline("Enter crop year in the format 2001 for 2001-02: ")
+    year <- readline("Enter year in the format 2001 for 2001-02: ")
   }
   year <- as.integer(year)
   cur_year <- paste(year, substr(year+1, 3, 4), sep = "-")
@@ -21,7 +37,7 @@ cost_component_graph <- function(data, year){
   # To check the created variables in the data set.
   req_crop_yr_var <- match(for_year_col, col_names)
   if(all(is.na(req_crop_yr_var)== TRUE))
-    stop("Mentioned crop year variable is missing")
+    stop("Mentioned year variable is missing")
   if(any(is.na(req_crop_yr_var) == TRUE))
     req_crop_yr_var <- as.integer(na.omit(req_crop_yr_var))
   
@@ -58,11 +74,13 @@ cost_component_graph <- function(data, year){
     melt(per_contribution[1:(num1-1),],
          id.vars = input_col_num)
   g <- ggplot(data_for_graphs, aes(x = variable, y = value, fill = Inputs)) +
-    geom_bar(position = "fill", stat = "identity", width = 0.3)
+    geom_bar(position = "fill", stat = "identity", width = 0.4)
   g <- g + scale_y_continuous(labels = scales::percent, 
                               breaks = scales::pretty_breaks(10))
   # storing data name as character for title
-  title_name <- deparse(substitute(data))
+  if(missing(title_name)){
+    title_name <- deparse(substitute(data))
+  }
   g <- g + ggtitle(paste(title_name,"- Total Cost with % share of each component"))
   g <- g + labs(x= NULL, y= NULL)
   shades_of_blue <- c("lightblue2","steelblue2","steelblue3",
@@ -71,6 +89,15 @@ cost_component_graph <- function(data, year){
                         "darkorange3", "darkorange4")
   color_name <- c(shades_of_orange, shades_of_blue)
   g <- g + scale_fill_manual(values = color_name)
+  g <- g +theme(
+    # panel.background = element_rect(linetype = "solid", fill =  "grey99" ),
+    panel.background = element_blank(),
+    panel.grid.major.y = element_line(linetype = "solid", color = "grey75"),
+    panel.grid.minor.y = element_blank(), 
+    panel.grid.major.x = element_blank(),
+    # axis.line.y = element_line(linetype = 1),
+    axis.text = element_text(face = "bold", colour = "black")
+  )
   
   # For annotation to be plotted above the bar
   data[, req_crop_yr_var] <- sapply(data[,req_crop_yr_var], 
@@ -89,7 +116,15 @@ cost_component_graph <- function(data, year){
     top_label <- final_label
   }
   g <- g + annotate("text", x = col_names[req_crop_yr_var], 
-               y = 1.05, label = top_label, size = 3)
-  g
+               y = 1.04, label = top_label, size = 3)
+  print(g)
+  note <- paste("Note:","Direct cost in shades of blue",
+                "Indirect cost in shades of orange", 
+                sep = "\n", collapse = "\n")
+  # Creating text on empty space of graph using grid.text from grid packages
+  grid.text(note, x = 0.7, y = 0.1, gp = gpar(cex = .7), just = "left")
+  # Storing the final graph to an R object
+  g <- recordPlot()
+  return(g)
 }
 
